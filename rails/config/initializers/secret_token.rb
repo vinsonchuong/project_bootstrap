040f51f8522ab2310193b8a040193ea7979861c5
Vinson Chuong <Vinson.Chuong@gmail.com>
2013-07-13 18:50:57 -0700

    Update secret_token initializer for Rails 4

--- rails/config/initializers/secret_token.rb
+++ rails/config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-AppName::Application.config.secret_token = ENV['SECRET_TOKEN']
+AppName::Application.config.secret_key_base = ENV['SECRET_TOKEN']
